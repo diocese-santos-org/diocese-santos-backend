d655fbce07359b443d538032ded3827fc104bfda
Leonardo Blume <leribeiro.blume@gmail.com>
2023-08-24 10:00:52 -0300

    feat: testando PR e comitando teste

---
+++ src/test/java/br/com/diocese/application/useCase/UsuarioUseCaseImplTest.java
@@ -0,0 +1,16 @@
+package br.com.diocese.application.useCase;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class UsuarioUseCaseImplTest {
+
+    @Test
+    public void testarSimples() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}
