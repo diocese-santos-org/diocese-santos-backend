79c3a1f34ad83c17554ef12095e8eaea1373ce6d
Leonardo Blume <leribeiro.blume@gmail.com>
2023-08-23 21:43:04 -0300

    Initial Commit

---
+++ src/main/java/br/com/diocese/DioceseSantosApplication.java
@@ -0,0 +1,13 @@
+package br.com.diocese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioceseSantosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioceseSantosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diocese/DioceseSantosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diocese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioceseSantosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
